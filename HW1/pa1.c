86699a0c7634a6f8b2128c1875daf195a4f8c132
alsichcan <alsichcan@naver.com>
2021-09-26 01:29:58 +0900

    Decoding correct

--- HW1/pa1.c
+++ HW1/pa1.c
@@ -55,7 +55,6 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
                         }
 
                         int mask = 1 << idx;
-
                         *(dst+length) = (*(dst+length) & ~mask) | (val << idx);
 
                         idx--;
@@ -150,5 +149,55 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
 /* TODO: Implement this function */
 int decode(const char* const src, const int srclen, char* const dst, const int dstlen)
 {
-    return 0;
+    if(srclen == 0) return 0;
+
+    // Decoding을 위한 변수
+    int i = 0; // Byte 단위의 위치
+    int j = 7; // Bit 단위의 위치
+    int curr_bit = 0;
+
+    // 메모리 쓰기를 위한 변수
+    int idx = 7; // Bit 단위의 위치
+    int length = 0; // Byte 단위의 위치
+
+    while(i < srclen){
+        // 3개 단위로 값을 읽어오기 --> 갯수(val)를 구한다.
+        int val = 0;
+        for (int pos = 2; pos >= 0; pos--) {
+            int bit = (*(src + i) >> j) & 1; // 앞에서부터 1 bit씩 추출
+            if (pos == 2) val += bit * 4;
+            else if (pos == 1) val += bit * 2;
+            else val += bit;
+            j--;
+            if (j < 0) {
+                j = 7;
+                i++;
+                // TODO : 마지막에 도달했을 떄? (if i == srclen?)
+                if(i == srclen)
+                    break;
+            }
+        }
+
+        if((length == dstlen) && val != 0)
+            return -1;
+
+        // 갯수(val)만큼 해당하는 bit을 입력한다.
+        while(val > 0){
+            int mask = 1 << idx;
+            *(dst + length) = (*(dst + length) & ~mask) | (curr_bit << idx);
+            //printf("%d번째 Byte %d번째 bit에 %d를 삽입 \n", length, idx, curr_bit);
+            idx--;
+            val--;
+
+            if (idx < 0) {
+                idx = 7;
+                length++;
+                if (length == dstlen && val != 0)
+                    return -1;
+            }
+        }
+        curr_bit = 1 - curr_bit; // bit 전환
+    }
+
+    return length;
 }
