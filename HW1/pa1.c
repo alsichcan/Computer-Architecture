4d9eb742d9eebc4cb70a6f633cd1efbd8d09602e
alsichcan <alsichcan@naver.com>
2021-09-26 02:03:28 +0900

    WriteBit Refactoring

--- HW1/pa1.c
+++ HW1/pa1.c
@@ -15,10 +15,13 @@
 //
 //---------------------------------------------------------------
 
-#include <stdio.h>
+int writeBit(int idx, char* dst, int val){
+    int mask = 1 << idx;
+    *dst = (*dst & ~mask) | (val << idx);
+    return 0;
+}
 
 int writeEncoding(int* count, int* idx, int* length, char* const dst, const int dstlen){
-    // TODO: Count 쓰기 (count = 0이 되도록)
     for(int pos = 2; pos >= 0; pos--){
         int val = 0;
         if(pos == 2){
@@ -34,8 +37,7 @@ int writeEncoding(int* count, int* idx, int* length, char* const dst, const int 
             *count -= val;
         }
 
-        int mask = 1 << *idx;
-        *(dst+*length) = (*(dst+*length) & ~mask) | (val << *idx);
+        writeBit(*idx, dst+*length, val);
 
         (*idx)--;
         if(*idx < 0){
@@ -45,11 +47,10 @@ int writeEncoding(int* count, int* idx, int* length, char* const dst, const int 
                 return -1;
         }
     }
-
     return 0;
 }
 
-/* TODO: Implement this function */
+
 int encode(const char* const src, const int srclen, char* const dst, const int dstlen)
 {
     if(srclen == 0) return 0;
@@ -87,20 +88,16 @@ int encode(const char* const src, const int srclen, char* const dst, const int d
     // 남은 Count 처리하기
     if (writeEncoding(&count, &idx, &length, dst, dstlen)==-1) return -1;
 
-
     // Padding 채우기
     while(idx >= 0){
-        int mask = 1 << idx;
-        *(dst+length) = (*(dst+length) & ~mask) | (0 << idx);
-
+        writeBit(idx, dst+length, 0);
         idx--;
         if(idx < 0) break;
     }
-
     return length + 1;
 }
 
-/* TODO: Implement this function */
+
 int decode(const char* const src, const int srclen, char* const dst, const int dstlen)
 {
     if(srclen == 0) return 0;
@@ -126,26 +123,28 @@ int decode(const char* const src, const int srclen, char* const dst, const int d
             if (j < 0) {
                 j = 7;
                 i++;
-                // TODO : 마지막에 도달했을 떄? (if i == srclen?)
-                if(i == srclen)
+                // 마지막에 도달했을 때 쓰레기 값을 읽어올 수 있으므로 loop을 종료한다.
+                if(i == srclen){
                     break;
+                }
             }
         }
 
+        // Padding (항상 val = 0)이외의 값이 있을경우 무시
         if((length == dstlen) && val != 0)
             return -1;
 
         // 갯수(val)만큼 해당하는 bit을 입력한다.
         while(val > 0){
-            int mask = 1 << idx;
-            *(dst + length) = (*(dst + length) & ~mask) | (curr_bit << idx);
-            //printf("%d번째 Byte %d번째 bit에 %d를 삽입 \n", length, idx, curr_bit);
+            writeBit(idx, dst+length, curr_bit);
             idx--;
             val--;
 
             if (idx < 0) {
                 idx = 7;
                 length++;
+
+                // Padding이 존재할 수 있으므로 length == dstlen만으로 return -1하면 안됨.
                 if (length == dstlen && val != 0)
                     return -1;
             }
