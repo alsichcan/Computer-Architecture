edacb868d1d511b65a6c7b4c5b20f0f02dd9c881
alsichcan <alsichcan@naver.com>
2021-11-14 20:36:57 +0900

    Compile Complete

--- HW3/bmpconv.s
+++ HW3/bmpconv.s
@@ -22,7 +22,7 @@
 	.globl bmpconv
 
 # --- Usage of registers ---
-main:
+bmpconv:
 # a0 ~ a5 value t0~t5로 이전
 # t0 : temporary imgptr
 # t1 : temporary h (h-2 ~ 0)
@@ -42,14 +42,22 @@ main:
     add a4, x0, x0
 
 # For loop : output[i][j]에 해당하는 pixel을 위한 convolution
-outer_loop_start:
-    beq t1, 0, outer_loop_end
-inner_loop_start:
-    beq t2, 0, inner_loop_end
+    addi sp, sp, -4
+    sw t2, 0(sp)
 
-    // TODO : kernel[p][q]에 해당하는 값 계산
-    addi sp, sp, -28
-    sw ra, 24(sp)
+outer_loop_start:
+    beq t1, x0, outer_loop_end
+inner_loop_start:
+    beq t2, x0, inner_loop_end
+
+    lw a2, 0(sp)
+# TODO : kernel[p][q]에 해당하는 값 계산
+# a0 : blue
+# a1 : green
+# a2 : red
+    addi sp, sp, -32
+    sw ra, 28(sp)
+    sw a3, 24(sp)
     sw a4, 20(sp)
     sw t0, 16(sp)
     sw t1, 12(sp)
@@ -63,52 +71,116 @@ inner_loop_start:
     lw t1, 12(sp)
     lw t0, 16(sp)
     lw a4, 20(sp)
-    lw ra, 24(sp)
+    lw a3, 24(sp)
+    lw ra, 28(sp)
+    addi sp, sp, 32
 
 
-    // TODO: *outptr에 값쓰기
-//    for (int bgr_count = 0; bgr_count < 3; bgr_count++){
-//        writeByte(outptr, write_bit_offset, saturate(bgr[bgr_count]));
-//        write_bit_offset++;
-//        if(write_bit_offset == 4){
-//            outptr++;
-//            write_bit_offset = 0;
-//        }
-//    }
+write_pixel:
+# TODO: *outptr에 값쓰기
+# t4 : output pointer
+# a4 : write_bit_offset
+# a3 : bgr_count
+# a2 : value
+    addi sp, sp, -20
+    sw ra, 16(sp)
+    sw a3, 12(sp)
+    sw a0, 8(sp) # blue
+    sw a1, 4(sp) # green
+    sw a2, 0(sp) # red
 
+    addi a3, x0, 2
+write_start:
+    blt a3, x0, write_end
+
+    bne a3, x0, write_b_or_g
+    lw a2, 0(sp)
+    beq x0, x0, rgb_written
+write_b_or_g:
+    addi a1, x0, 1
+    bne a4, a1, write_blue
+    lw a2, 4(sp)
+    beq x0, x0, rgb_written
+write_blue:
+    lw a2, 8(sp)
+    beq x0, x0, rgb_written
+
+rgb_written:
+    jal ra, writeImage
+    addi a4, a4, 1
+    addi a0, x0, 4
+    bne a4, a0, write_continue
+    addi t4, t4, 1
+    add a4, x0, x0
+
+write_continue:
+    addi a3, a3, -1
+    beq x0, x0, write_start
+
+write_end:
+    lw a3, 12(sp)
+    lw ra, 16(sp)
+    addi sp, sp, 20
+
+
+# TODO: read_bit_offset, imgptr update
+    addi a2, x0, 3
+update:
+    beq a2, x0, next_pixel
+    addi a2, a2, -1
+    addi a3, a3, 1
+    addi a1, x0, 4
+    bne a3, a1, update
+    addi t0, t0, 1
+    add a3, x0, x0
+    beq x0, x0, update
+
+next_pixel:
     addi t2, t2, -1
     beq x0, x0, inner_loop_start
 
 inner_loop_end:
-
-    // TODO : 한행의 처리가 모두 끝났을 때
-    // TODO : padding 처리
-//    while(write_bit_offset < 4){
-//        writeByte(outptr, write_bit_offset, 0);
-//        write_bit_offset++;
-//    }
-    addi t0, t0, 1
-    add a1, x0, x0
+# TODO : 한행의 처리가 모두 끝났을 때 (padding 처리)
+# TODO : write padding
+write_padding:
+    addi a1, x0, 4
+    bge a4, a1, arg_update
     add a2, x0, x0
+
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    jal ra, writeImage
+    lw ra, 0(sp)
+    addi sp, sp, 4
+
+    addi a4, a4, 1
+    beq x0, x0, write_padding
+
+arg_update:
+# TODO : 변수 update
+    add a3, x0, x0
     add a4, x0, x0
-    addi t4, t4, -1
+    addi t0, t0, 1
+    addi t1, t1, -1
+    lw t2, 0(sp)
+    addi t4, t4, 1
     beq x0, x0, outer_loop_start
 
 outer_loop_end:
-    // TODO : 후처리? (restore sp, ra)
+    addi sp, sp, 4
     jalr x0, 0(ra)
 
 calPixel:
 # a0 : temp
 # a1 : temp
 # a2 : temp (kernel, byte)
-# a3 (*) : read_bit_offset
-# a4 (*) : bgr_count
-# t0 (*) : temporary imgptr & byte_offset
-# t1 (*) : kernel[p]
-# t2 (*) : kernel[q]
-# t3 (*) : kernel pointer
-# t4 (*) : kernel offset
+# a3 (*) : read_bit_offset --> temporary read_bit_offset
+# a4 (*) : write_bit_offset --> bgr_count
+# t0 (*) : imgptr --> temporary imgptr & byte_offset
+# t1 (*) : temporary h --> kernel[p]
+# t2 (*) : temporary w --> kernel[q]
+# t3 (*) : kernel pointer --> temporary kernel pointer
+# t4 (*) : output pointer --> kernel offset
 
 # w_byte 선계산하기 (a2 후에 stack에 넣기)
     srli a1, a2, 2
@@ -120,20 +192,15 @@ calPixel:
     add a2, a2, a1
 
 # Stack Memory에 추가하기
-    addi sp, sp, -40
-    sw ra, 36(sp) # address
-    sw a2, 32(sp) # w_byte
-    sw a3, 28(sp) # read_bit_offset
-    sw a4, 24(sp) # write_bit_offset
-    sw t0, 20(sp) # imgptr
-    sw t3, 16(sp) # kerptr
-    sw t4, 12(sp) # writeptr
+    addi sp, sp, -32
+    sw ra, 28(sp) # address
+    sw a2, 24(sp) # w_byte
+    sw a3, 20(sp) # read_bit_offset
+    sw t0, 16(sp) # imgptr
+    sw t3, 12(sp) # kerptr
     sw x0, 8(sp) # blue
     sw x0, 4(sp) # green
     sw x0, 0(sp) # red
-
-#  초기화
-    add a0, x0, x0
 
 # p, q, kernel_offset 초기화
     addi t1, x0, 3
@@ -141,10 +208,10 @@ calPixel:
     add t4, x0, x0
 
 outer_pixel_start:
-    beq a4, 0, outer_pixel_end
-    lw t0, 20(sp)
-    lw a1, 32(sp)
-    lw a3, 28(sp)
+    beq t1, x0, outer_pixel_end
+    lw t0, 16(sp) # *imgptr
+    lw a3, 20(sp) # read_bit_offset
+    lw a1, 24(sp) # w_byte
     sub a0, x0, t1
     addi a0, a0, 3
 mul_w_byte:
@@ -160,12 +227,17 @@ inner_pixel_start:
     beq a2, x0, rgb_ignored
     addi a4, x0, 2
 rgb_start:
-    // TODO : rgb value읽어오기
+# TODO : rgb value읽어오기
     blt a4, x0, rgb_end
+    addi sp, sp, -4
+    sw a2, 0(sp) # kernel 값 저장
     jal ra, readImage
+    lw a2, 0(sp)
+    addi sp, sp, 4
 
-    bge a2, x0, 2
+    bge a2, x0, pixel_value
     xori a0, a0, -1
+pixel_value:
     add a2, a0, x0
 
     bne a4, x0, b_or_g
@@ -197,14 +269,14 @@ rgb_continue:
 
 rgb_ignored: # read_bit_offset update
     addi a1, x0, 3
-rgb_ignored_loop
-    beq a1, x0, rgb_end
-    addi a3, a3, 1
     addi a0, x0, 4
+rgb_ignored_loop:
+    beq a1, x0, rgb_end
+    addi a1, a1, -1
+    addi a3, a3, 1
     bne a3, a0, rgb_ignored_loop
     addi t0, t0, 1
     add a3, x0, x0
-    addi a1, a1, -1
     beq x0, x0, rgb_ignored_loop
 
 rgb_end: # kernel_offset update
@@ -219,46 +291,16 @@ pixel_end:
     beq x0, x0, inner_pixel_start
 
 inner_pixel_end:
-    addi a3, a3, -9
     addi t1, t1, -1
+    addi t2, x0, 3
     beq x0, x0, outer_pixel_start
 
 outer_pixel_end:
-    // TODO : *outptr에 값 쓰기
-    addi a4, x0, 2
-write_start:
-    blt a4, x0, next_pixel
-
-    bne a4, x0, b_or_g
-    lw a3, 0(sp)
-    beq x0, x0, rgb_written
-b_or_g:
-    addi a1, x0, 1
-    bne a4, a1, blue
-    lw a3, 4(sp)
-    beq x0, x0, rgb_written
-blue:
-    lw a3, 8(sp)
-    beq x0, x0, rgb_written
-
-rgb_written:
-    sw a4, 24(sp) # write_bit_offset
-    sw t4, 12(sp) # writeptr
-    jal ra writeImage
-    addi a3, a3, 1
-    addi a0, x0, 4
-    bne a3, a0, write_continue
-    addi t0, t0, 1
-    add a3, x0, x0
-
-write_continue
-    addi a4, a4, -1
-    beq x0, x0, write_start
-
-next_pixel
-    // TODO: 한 pixel의 처리가 끝났을 때
-    lw ra, 32(sp)
-    addi sp, sp, 36
+    lw ra, 28(sp)
+    lw a0, 8(sp)
+    lw a1, 4(sp)
+    lw a2, 0(sp)
+    addi sp, sp, 32
     jalr x0, 0(ra)
 
 
@@ -269,8 +311,9 @@ readKernel:
     addi a0, x0, 0x000000FF
     slli a1, t4, 4
     sll a0, a0, a1
-    and a0, a0, t3
-    srl a0, a0, t4
+    lw a2, 0(t3)
+    and a0, a0, a2
+    srl a0, a0, a1
     jalr x0, 0(ra)
 
 # Imageptr에서 1byte 읽어오기 (a0에 저장)
@@ -280,31 +323,32 @@ readImage:
     addi a0, x0, 0x000000FF
     slli a1, a3, 4
     sll a0, a0, a1
-    lw a1, 0(t0)
-    and a0, a0, a1
-    srl a0, a0, a3
+    lw a2, 0(t0)
+    and a0, a0, a2
+    srl a0, a0, a1
     jalr x0, 0(ra)
 
-# Outptr에 1byte 쓰기 (a3에 저장)
-# a3 : value
+# Outptr에 1byte 쓰기 (a2에 저장)
+# a2 : value
 # a4 : write_bit_offset
 # t4 : outptr
 writeImage:
     addi a0, x0, 0x000000FF
-    slli a1, t4, 4
+    slli a1, a4, 4
     sll a0, a0, a1
     xori a0, a0, -1
     lw a1, 0(t4)
     and a0, a0, a1
 # Saturate
-bge a3, x0, 2
-    add a3, x0, x0
+    bge a2, x0, greater_0
+    add a2, x0, x0
+greater_0:
     addi a1, x0, 255
-    blt a3, a1, value_saturated
-    addi a3, x0, 255
+    blt a2, a1, value_saturated
+    addi a2, x0, 255
 value_saturated:
-    slli a1, t4, 4
-    sll a1, a3, a1
+    slli a1, a4, 4
+    sll a1, a2, a1
     or a0, a0, a1
     sw a0, 0(t4)
     jalr x0, 0(ra)
